54da9812893c39016ac85bbdc3ad319f17e64134
Cheyanne Kester <ckester@uoregon.edu>
2022-10-10 16:23:13 -0700

    Set up application
    generated a new angular app, and got a nav bar running with the pages, nothing crazy yet

---
+++ ProjectApp/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HighScoresComponent } from './high-scores/high-scores.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TestModelComponent } from './test-model/test-model.component';
+
+const routes: Routes = [
+  {path:  'home', component: HomePageComponent},
+  {path:  'high-scores', component: HighScoresComponent},
+  {path:  'test-model', component: TestModelComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
