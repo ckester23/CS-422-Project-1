58b0b1df243599ea7ca83657162470dcb551cc3f
Cheyanne Kester <ckester@uoregon.edu>
2022-10-11 16:29:39 -0700

    created placeholder design for pages

--- ProjectApp/src/app/test-model/test-model.component.ts
+++ ProjectApp/src/app/test-model/test-model.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-test-model',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestModelComponent implements OnInit {
 
-  constructor() { }
+  testForm = this.formBuilder.group( {
+    name:'',
+    answer: '',
+    gitURL: ''
+  });
+
+  public currentUser!: Object;
+
+  public userString!: String;
+
+  constructor( private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  public onSubmit() {
+    // save user input data
+
+    //might make a formal global object for users
+    this.currentUser = {
+      name: (this.testForm.controls['name'].value), 
+      answer: (this.testForm.controls['answer'].value),
+      gitURL: (this.testForm.controls['gitURL'].value)
+    };
+    
+    this.userString = (this.testForm.controls['name'].value) + ' ' +
+    (this.testForm.controls['answer'].value) + ' ' +
+    (this.testForm.controls['gitURL'].value);
+    
+    console.warn('You clicked submit', this.currentUser);
+
+    this.testForm.reset;
   }
 
 }
