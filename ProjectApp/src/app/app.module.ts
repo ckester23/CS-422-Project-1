bb938580e8fd9a992b625ddefc008362691cfb07
ckester23 <ckester@uoregon.edu>
2022-10-21 12:40:05 -0700

    quick commit?

--- ProjectApp/src/app/app.module.ts
+++ ProjectApp/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { HighScoresComponent } from './components/high-scores/high-scores.component';
+import { TestModelComponent } from './components/test-model/test-model.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponent } from './components/header/header.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { DatabasesComponent } from './components/databases/databases.component';
 import { ScoresService } from './services/scores.service';
@@ -15,7 +20,9 @@ import { ScoresService } from './services/scores.service';
     HighScoresComponent,
     TestModelComponent,
     NavBarComponent,
+    HeaderComponent,
     MessagesComponent,
+    DatabasesComponent,
   ],
   imports: [
     BrowserModule,
