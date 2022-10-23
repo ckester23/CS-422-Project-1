c6ef19f98bcfc15b7baea756842946e71aa2a976
Scott Wallace <scottytridude@gmail.com>
2022-10-23 13:02:42 -0700

    implemented meta data for frame and begun ts implementation

--- ProjectApp/src/app/components/databases/databases.component.ts
+++ ProjectApp/src/app/components/databases/databases.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Frame } from 'src/app/frame';
-import { FrameService } from 'src/app/services/frame.service';
 
 @Component({
   selector: 'app-databases',
@@ -12,21 +11,16 @@ export class DatabasesComponent implements OnInit {
   title: string = 'Dux D-Zine Time Series Database'
   about: string = 'check out all the cool time series below!'
 
-  frames: Frame[] = [];
-  selectedFrame?: Frame;
+  // dummy ts
+  tsFrames: Frame[] = [{
+    ts: [],
+    setName: "foo",
+    isSet: false,
+  }]
 
-  getFrames(): void {
-    this.frameService.getFrames().subscribe(frames => this.frames = frames);
-  }
-
-  onSelect(frame: Frame): void {
-    this.selectedFrame = frame;
-  }
-
-  constructor(private frameService: FrameService) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.getFrames();
   }
 
 }
--- ProjectApp/src/app/frame.ts
+++ ProjectApp/src/app/frame.ts
@@ -1,14 +1,46 @@
 /* frame interface for storing datasets
-and dataset meta data */
+and dataset meta data 
+
+-assumes all ts are set/heirarch
+-non heirarch is just single ts
+*/
+
+
 export interface Frame {
-    // name of dataset
-    frameTitle: string;
-    // file path to dataset
-    framePath: string;
-    // necceseary request to get data
-    frameRequest?: string;
-    // metadata
-    frameMeta?: string[];
-    // local storage for dataset
-    frameData?: object;
+    ts: TS[];
+    setName: string;
+    Description?: string;
+    applicationDomains?: string;
+    Keywords?: string;
+    isSet: boolean;
+    // (1 for univariate, >1 for multivariate)
+    vectorSize?: number;
+    minLength?: number;
+    maxLength?: number;
+    // size of ts array
+    tsAmmount?: number;
+    isComponent?: number;
+    contributors?: string;
+    relatedPaperReference?: string;
+    relatedPaperLink?: string;
+}
+
+
+export interface TS {
+    // meta data
+    tsName: string;
+    description?: string;
+    domain?: string;
+    units?: string;
+    keywords?: string[];
+    // Scalar/vector (univariate/multivariate)
+    tsParentType?: string;
+    vectorSize?: number;
+    length?: number;
+    samplingPeriod?: string;
+    // ts data
+    // potentailly just request and no real 
+    // ts data
+    data?: string;
+
 }
